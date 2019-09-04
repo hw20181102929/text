db9f33b39a5959aedce4fddf7ac018c15762cda5
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-09-04 11:59:07 +0800

    swift
    swift

--- text/main.swift
+++ text/main.swift
@@ -1,12 +1,23 @@
-//
-//  main.swift
-//  text
-//
-//  Created by s20181102929 on 2019/9/4.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+print("请输入用户名：");
+var name=readLine()!;
+if name == "admin"{
+print("请输入密码：")
+var mima:String=readLine()!
+if mima == "888888"{
+    print("yes!")
+}
+else {
+    print("no!")
+}
+}
+else{
+    print("请输入密码：")
+    var Mima:String=readLine()!
+    print("用户不存在：")
+    print("密码错误 ")
+}
+var x=1
+while x<10{
+    print("*")
+    x=x+1;
+}

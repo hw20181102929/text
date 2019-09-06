3143d653fc7fce29b54358fe25e3a9c5c7cd641b
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-09-06 08:38:25 +0800

    swift
    swuft

--- text/main.swift
+++ text/main.swift
@@ -1,14 +1,14 @@
 print("请输入用户名：");
 var name=readLine()!;
 if name == "admin"{
-print("请输入密码：")
-var mima:String=readLine()!
-if mima == "888888"{
-    print("yes!")
-}
-else {
-    print("no!")
-}
+    print("请输入密码：")
+    var mima:String=readLine()!
+    if mima == "888888"{
+        print("yes!")
+    }
+    else {
+        print("no!")
+    }
 }
 else{
     print("请输入密码：")
@@ -16,8 +16,14 @@ else{
     print("用户不存在：")
     print("密码错误 ")
 }
-var x=1
-while x<10{
-    print("*")
-    x=x+1;
+var n:Int = readLine()!
+switch (n){
+case 1:print("*")
+break;
+case 2:
+ print("**")
+break
+default :
+   print("***")
+   break;
 }
